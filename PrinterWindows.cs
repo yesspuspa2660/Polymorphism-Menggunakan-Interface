aa265728c1973cc90a0fa59a97c4886b6047e4e7
yesspuspa2660 <61943093+yesspuspa2660@users.noreply.github.com>
2020-05-13 20:12:01 +0700

    Add files via upload
    Polymorphism Menggunakan Interface

---
+++ PrinterWindows.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePrinter
+{
+    public interface PrinterWindows
+    {
+        void Show();
+        void Print();
+    }
+
+    public class Epson : PrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("Epson display dimension : 10*11");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Epson printer printing......");
+        }
+    }
+    public class Canon : PrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("Canon display dimension : 9.5*12");
+        }
+        public void Print()
+        {
+            Console.WriteLine("Canon printer printing......");
+        }
+    }
+    public class LaserJet : PrinterWindows
+    {
+        public void Show()
+        {
+            Console.WriteLine("LaserJet display dimension : 12*12");
+        }
+        public void Print()
+        {
+            Console.WriteLine("LaserJet printer printing......");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama \t: Rahayu Puspa Indah Lestari Ningrum");
+            Console.WriteLine("NIM \t: 19.11.2660");
+            Console.WriteLine("Kelas \t: Informatika 02\n");
+
+
+            PrinterWindows printer;
+            
+            Console.WriteLine("Pilih Printer : ");
+            Console.WriteLine("1. Epson");
+            Console.WriteLine("2. Canon");
+            Console.WriteLine("3. LaserJet");
+
+            Console.Write("Pilih printer yang akan digunakan [1/2/3] : ");
+
+            int NomorPrinter = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+
+            if (NomorPrinter == 1)
+            {
+                printer = new Epson();
+            }
+            else if (NomorPrinter == 2)
+            {
+                printer = new Canon();
+            }
+            else
+            {
+                printer = new LaserJet();
+            }
+
+            printer.Show();
+            printer.Print();
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
